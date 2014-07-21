87044a16a1ef22b48a600a071b9383a284fe08d0
Jason Thompson <jason@jthompson.ca>
2014-07-21 16:32:32 -0400

    ongoing

--- kata4/src/weather.rs
+++ kata4/src/weather.rs
@@ -1,7 +1,7 @@
-#![crate_name = "weather"]
-
 #![feature(globs)]
+
 use std::num;
+use std::io::File;
 
 fn sanitize<'a>(unsanitized: &'a str) -> &'a str {
   if unsanitized.ends_with("*") {
@@ -16,19 +16,30 @@ fn to_num(str_num: &str) -> f32 {
   f_num
 }
 
-fn parse_line(line: Vec<&str>) {
-  let v: Vec<&str> = line.words().collect();
+fn parse_line<'a>(line: &'a str) -> Option<Vec<f32>> {
+  if line.is_empty() || !line.char_at(0).is_digit() {
+    None
+  } else {
+    let v: Vec<f32> = line.words().map(|x| to_num(sanitize(x))).collect();
+    Some(v)
+  }
+}
 
-  if v.is_empty() || !v[0].char_at(0).is_digit() {
-    return
-  };
+fn main() {
+  let path = Path::new("assets/weather.dat");
+  let mut file = File::open(&path);
+  let data = file.read_to_end().unwrap();
+  let data_string = String::from_utf8(data);
 
-  // let day = Day::new(v);
-  v 
+  for line in data_string.unwrap().as_slice().lines().unwrap() {
+    match parse_line(line) {
+      Some(x) => x,
+      None => {}
+    };
+  }
 }
 
 mod tests {
-
   #[test]
   fn test_sanitize() {
     assert_eq!("32".as_slice(), super::sanitize("32*".as_slice()));
@@ -42,7 +53,13 @@ mod tests {
 
   #[test]
   fn test_parse_line() {
-    let line = "4  77    59    68          51.1       0.00         110  9.1 130  12  8.6  62 40 1021.1".as_slice();
-    assert_eq!(, super::parse_line(line));
+    let line = "4  77    59    68          51.1       0.00         110  9.1 130  12  8.6  62 40 1021.1";
+    let line_vec = vec!(4.0f32, 77.0, 59.0, 68.0, 51.1, 0.00, 110.0, 9.1, 130.0, 12.0, 8.6, 62.0, 40.0, 1021.1);
+
+    assert_eq!(line_vec, super::parse_line(line).unwrap());
+    assert_eq!(None, super::parse_line("".as_slice()));
+
+    let tricky_line = "26  97*   64";
+    assert_eq!(vec!(26.0f32, 97.0, 64.0), super::parse_line(tricky_line).unwrap());
   }
 }
